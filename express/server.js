2d3805b6962f668d67c0a6061e05633a9616bb53
netti-w <106514885+netti-w@users.noreply.github.com>
2023-01-05 14:28:27 +0100

    Update server.js

--- express/server.js
+++ express/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const uuid = require('uuid');
 const { check, validationResult } = require('express-validator');
 
-//const mongoose = require('mongoose'); // require (import) mongoose package
+const mongoose = require('mongoose'); // require (import) mongoose package
 // const Models = require('../models.js'); //require (import) 'custom' models.js
 
 // Connecting LOCAL myFlixDB via Mongoose to perform CRUD operations
